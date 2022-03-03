f4caa56e7e6c11c227fc4a7c00f423f3d761cc87
David S <dvdsanar@gmail.com>
2022-03-03 17:03:57 +0100

    imagen fin de combate y arreglo en barras de vida

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@ let pantalla1 = document.getElementById("inicio");
 let pantalla2 = document.getElementById("seleccion");
 let pantalla3 = document.getElementById("tercera");
 let luchar = document.getElementById("luchar");
+let ganador = document.getElementById("final");
 const cambioPantalla = () => {
   if (
     pantalla1.style.display === "flex" &&
@@ -50,7 +51,6 @@ const retraso = () => {
   pantalla3.style.display = "flex";
 };
 const cambioPantalla2 = () => {
-  console.log(players);
   if (players.length == 1) {
     switch (players[0].nombre) {
       case "Mario":
@@ -111,6 +111,19 @@ const cambioPantalla2 = () => {
     setTimeout(retraso, 500);
   }
 };
+const ganaP1 = () => {
+  ganador.style.backgroundImage = "url('/images/player1.jpg')";
+  ganador.style.display = "block";
+  pantalla3.removeAttribute("tabindex");
+  document.getElementById("vida2").style.width = "0%";
+};
+
+const ganaP2 = () => {
+  ganador.style.display = "block";
+  ganador.style.backgroundImage = "url('/images/player2.jpg')";
+  pantalla3.removeAttribute("tabindex");
+  document.getElementById("vida1").style.width = "0%";
+};
 const teclaPulsada = (event) => {
   let e = event.keyCode;
 
@@ -118,35 +131,52 @@ const teclaPulsada = (event) => {
     case 97:
       players[1].vida = players[1].vida - players[0].ataque;
       document.getElementById("vida2").style.width = players[1].vida + "%";
-      console.log("vida" + players[1].vida);
+      if (players[1].vida <= 0) {
+        ganaP1();
+      }
       break;
     case 115:
       players[0].ataque = players[0].ataque + 0.5;
       players[1].vida = players[1].vida - players[0].ataque;
       document.getElementById("vida2").style.width = players[1].vida + "%";
       players[0].ataque = players[0].ataque - 0.5;
+      if (players[1].vida <= 0) {
+        ganaP1();
+      }
       break;
     case 100:
       players[0].ataque = players[0].ataque + 1;
       players[1].vida = players[1].vida - players[0].ataque;
       document.getElementById("vida2").style.width = players[1].vida + "%";
       players[0].ataque = players[0].ataque - 1;
+      if (players[1].vida <= 0) {
+        ganaP1();
+      }
       break;
     case 106:
       players[0].vida = players[0].vida - players[1].ataque;
       document.getElementById("vida1").style.width = players[0].vida + "%";
+      if (players[0].vida <= 0) {
+        ganaP2();
+      }
       break;
     case 107:
       players[1].ataque = players[1].ataque + 0.5;
       players[0].vida = players[0].vida - players[1].ataque;
       document.getElementById("vida1").style.width = players[0].vida + "%";
       players[1].ataque = players[1].ataque - 0.5;
+      if (players[0].vida <= 0) {
+        ganaP2();
+      }
       break;
     case 108:
       players[1].ataque = players[1].ataque + 1;
       players[0].vida = players[0].vida - players[1].ataque;
       document.getElementById("vida1").style.width = players[0].vida + "%";
       players[1].ataque = players[1].ataque - 1;
+      if (players[0].vida <= 0) {
+        ganaP2();
+      }
       break;
   }
 };
